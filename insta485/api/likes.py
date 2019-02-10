0c0219fedb7707180e456beeed35c8519d65fbf6
jbbe <jbbe@umich.edu>
2019-02-10 17:56:58 -0500

    implemented like and unlike apis

--- insta485/api/likes.py
+++ insta485/api/likes.py
@@ -1,9 +1,10 @@
 """REST API for likes."""
 import flask
 import insta485
+from insta485.model import get_db
 
 
-@insta485.app.route('/api/v1/p/<int:postid_url_slug>/likes/', methods=["GET"])
+@insta485.app.route('/api/v1/p/<int:postid_url_slug>/likes/', methods=["GET", "POST", "DELETE"])
 def get_likes(postid_url_slug):
     """Return likes on postid.
 
@@ -15,6 +16,7 @@ def get_likes(postid_url_slug):
       "url": "/api/v1/p/1/likes/"
     }
     """
+
     if "username" not in flask.session:
         flask.abort(403)
 
@@ -51,5 +53,43 @@ def get_likes(postid_url_slug):
     likes_count = cur.fetchone()
     context.update(likes_count)
 
+    # Unlike post
+    if flask.request.method == 'DELETE':
+        if(logname_likes_this['logname_likes_this'] == 1):
+            sql_delete_like = "delete from likes where owner = \'"
+            sql_delete_like += logname + "\' and postid = "
+            sql_delete_like += str(postid)
+            get_db().cursor().execute(sql_delete_like)
+        return flask.jsonify(), 204
+
+    # Add like
+    if flask.request.method == 'POST':
+        # Check if user already likes post
+        # If so return 409 conflict
+        print(type(logname_likes_this), logname_likes_this['logname_likes_this'] )
+        if(logname_likes_this['logname_likes_this'] == 1):
+            return flask.jsonify({'logname': logname, 'message': "Conflict", "postid": postid, "status_code":409}), 409
+        else:
+            # Insert like into database
+            sql_insert_like = "insert into likes(owner,postid) values(\'"
+            sql_insert_like += logname + "\',"
+            sql_insert_like += str(postid) + ")"
+            get_db().cursor().execute(sql_insert_like)
+
+            # If like was succesfully inserted and return 201 if it worked
+            cur = connection.execute(
+                "SELECT EXISTS( "
+                "  SELECT 1 FROM likes "
+                "    WHERE postid = ? "
+                "    AND owner = ? "
+                "    LIMIT 1"
+                ") AS logname_likes_this ",
+                (postid, logname)
+            )
+            logname_likes_this = cur.fetchone()
+            # if(logname_likes_this):
+            return 201
+            # Otherwise return error
     return flask.jsonify(**context)
 
+    
