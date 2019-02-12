853e623540f180539e3f89acccc7a89d973b054c
vivs <vivs@umich.edu>
2019-02-12 00:47:21 -0500

    did first 5 get links for posts.py

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -1,4 +1,6 @@
 """Insta485 REST API."""
 
 from insta485.api.likes import get_likes
-
+from insta485.api.posts import get_posts
+from insta485.api.posts import get_post_metadata
+from insta485.api.posts import get_links
