39c4286856ff092f135df86f59db84c90a08a6a1
jbbe <jbbe@umich.edu>
2019-02-12 17:45:43 -0500

    fixed all style issues

--- insta485/api/likes.py
+++ insta485/api/likes.py
@@ -4,10 +4,10 @@ import insta485
 from insta485.model import get_db
 
 
-@insta485.app.route('/api/v1/p/<int:postid_url_slug>/likes/', methods=["GET", "POST", "DELETE"])
+@insta485.app.route('/api/v1/p/<int:postid_url_slug>/likes/',
+                    methods=["GET", "POST", "DELETE"])
 def get_likes(postid_url_slug):
     """Return likes on postid.
-
     Example:
     {
       "logname_likes_this": 1,
@@ -55,7 +55,7 @@ def get_likes(postid_url_slug):
 
     # Unlike post
     if flask.request.method == 'DELETE':
-        if(logname_likes_this['logname_likes_this'] == 1):
+        if logname_likes_this['logname_likes_this'] == 1:
             sql_delete_like = "delete from likes where owner = \'"
             sql_delete_like += logname + "\' and postid = "
             sql_delete_like += str(postid)
@@ -66,30 +66,27 @@ def get_likes(postid_url_slug):
     if flask.request.method == 'POST':
         # Check if user already likes post
         # If so return 409 conflict
-        print(type(logname_likes_this), logname_likes_this['logname_likes_this'] )
-        if(logname_likes_this['logname_likes_this'] == 1):
-            return flask.jsonify({'logname': logname, 'message': "Conflict", "postid": postid, "status_code":409}), 409
-        else:
-            # Insert like into database
-            sql_insert_like = "insert into likes(owner,postid) values(\'"
-            sql_insert_like += logname + "\',"
-            sql_insert_like += str(postid) + ")"
-            get_db().cursor().execute(sql_insert_like)
+        if logname_likes_this['logname_likes_this'] == 1:
+            return flask.jsonify({'logname': logname, 'message': "Conflict",
+                                  'postid': postid, 'status_code': 409}), 409
+        # Insert like into database
+        sql_insert_like = "insert into likes(owner,postid) values(\'"
+        sql_insert_like += logname + "\',"
+        sql_insert_like += str(postid) + ")"
+        get_db().cursor().execute(sql_insert_like)
 
-            # If like was succesfully inserted and return 201 if it worked
-            cur = connection.execute(
-                "SELECT EXISTS( "
-                "  SELECT 1 FROM likes "
-                "    WHERE postid = ? "
-                "    AND owner = ? "
-                "    LIMIT 1"
-                ") AS logname_likes_this ",
-                (postid, logname)
-            )
-            logname_likes_this = cur.fetchone()
-            # if(logname_likes_this):
-            return 201
-            # Otherwise return error
+        # If like was succesfully inserted and return 201 if it worked
+        cur = connection.execute(
+            "SELECT EXISTS( "
+            "  SELECT 1 FROM likes "
+            "    WHERE postid = ? "
+            "    AND owner = ? "
+            "    LIMIT 1"
+            ") AS logname_likes_this ",
+            (postid, logname)
+        )
+        logname_likes_this = cur.fetchone()
+        # if(logname_likes_this):
+        return 201
+        # Otherwise return error
     return flask.jsonify(**context)
-
-    
