611a024d5085f891e8fdb7a1b3c1ee79be45360b
David Yu <davidfyu@umich.edu>
2019-02-14 13:47:09 -0500

    add rest api paths for GET and POST comments

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -3,3 +3,4 @@ from insta485.api.likes import get_likes
 from insta485.api.posts import get_posts
 from insta485.api.posts import get_post_metadata
 from insta485.api.posts import get_links
+from insta485.api.comments import get_comments
---
+++ insta485/api/comments.py
@@ -0,0 +1,39 @@
+"""REST API for comments."""
+import flask
+import insta485
+
+@insta485.app.route('/api/v1/p/<int:postid_url_slug>/comments/', methods=["GET", "POST"])
+def get_comments(postid_url_slug):
+
+	if "username" not in flask.session:
+		flask.abort(403)
+	
+	logname = flask.session["username"]
+	connection = insta485.model.get_db()
+
+	if flask.request.method == 'POST':
+		
+		post_text = flask.json.loads(flask.request.data)['text']
+		sql_query = "insert into comments(owner,postid,text) values(\'"
+		sql_query += logname + "\',"
+		sql_query += str(postid_url_slug) + ", \'"
+		sql_query += post_text + "\')"
+		cur = connection.execute(sql_query)
+		
+		sql_query = "select commentid, owner, owner as owner_show_url, {0} as postid, text from comments where rowid = last_insert_rowid()".format(postid_url_slug)
+		cur = connection.execute(sql_query)
+		comment = cur.fetchone()
+		comment['owner_show_url'] = '/u/' + comment['owner_show_url'] + '/'
+
+		return flask.jsonify(**comment), 201
+
+	context = {}
+	context["url"] = flask.request.path
+	sql_query = "select commentid, owner, owner as owner_show_url, {0} as postid, text from comments where postid = {0}".format(postid_url_slug)
+	cur = connection.execute(sql_query)
+	comments = cur.fetchall()
+	for c in comments:
+		c['owner_show_url'] = '/u/' + c['owner_show_url'] + '/'
+	context["comments"] = comments
+
+	return flask.jsonify(**context)
