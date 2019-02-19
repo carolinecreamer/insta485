a5b3e92e72371442eaf10a026e5422c26152475e
vivs <vivs@umich.edu>
2019-02-19 09:18:10 -0500

    it is loading the fetchdata function but cannot access this.state items

--- insta485/js/index.jsx
+++ insta485/js/index.jsx
@@ -25,22 +25,25 @@ class Index extends React.Component {
   		.catch(error => console.log(error));
   	}
    fetchData() {
-        fetch(this.state.next, { credentials: 'same-origin' })
-              .then((response) => {
-                  if (!response.ok) throw Error(response.statusText);
-                  return response.json();
-                })
-                .then((data) => {
-                  this.state.results.concat(data.results)
-                })
+        fetch(this.link, { credentials: 'same-origin' })
+        .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+              this.results.concat(data.results);
+          })
+          .catch(error => console.log(error));
     }
+    
   	render() {
   		return (
         <div>
         <InfiniteScroll
-          dataLength = {this.state.results.length}
-          hasMore= {this.state.next !== ""}
-          loadMore = {fetchData}
+          dataLength={this.state.results.length}
+          hasMore={this.state.next !== ""}
+          next={this.fetchData}
+          link={this.state.next}
+          results={this.state.results}
         >
   			<div className="index">
 	  			{this.state.results.map((item, index) => (
